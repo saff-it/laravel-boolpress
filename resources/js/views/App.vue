693956b3a84df95719e67109b82223f9d1e83302
saff-it <saff_it@ymail.com>
2022-09-29 16:22:03 +0200

    App.vue + Header updated

--- resources/js/views/App.vue
+++ resources/js/views/App.vue
@@ -1,16 +1,16 @@
 <template>
   <div>
-        <MainComponent/>>
+        <HeaderComponent/>>
   </div>
 </template>
 
 <script>
-    import MainComponent from '../components/MainComponent.vue';
+    import HeaderComponent from '../components/HeaderComponent.vue';
 
 export default {
     
     components: {
-        MainComponent,
+        HeaderComponent,
     }
 }
 </script>
