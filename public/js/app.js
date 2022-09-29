f4ca5402771c8fb82dfd6b67c4dbcf9c6b29834d
saff-it <saff_it@ymail.com>
2022-09-29 15:34:30 +0200

    js update

--- public/js/app.js
+++ public/js/app.js
@@ -37367,8 +37367,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\saff_\Desktop\Back End\Laravel\laravel-api\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\saff_\Desktop\Back End\Laravel\laravel-api\resources\sass\app.scss */"./resources/sass/app.scss");
+__webpack_require__(/*! C:\Users\saff_\Desktop\Back End\Laravel\laravel-boolpress\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\saff_\Desktop\Back End\Laravel\laravel-boolpress\resources\sass\app.scss */"./resources/sass/app.scss");
 
 
 /***/ })
--- public/js/front.js
+++ public/js/front.js
@@ -14567,7 +14567,7 @@ __webpack_require__.r(__webpack_exports__);
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-module.exports = __webpack_require__(/*! C:\Users\saff_\Desktop\Back End\Laravel\laravel-api\resources\js\front.js */"./resources/js/front.js");
+module.exports = __webpack_require__(/*! C:\Users\saff_\Desktop\Back End\Laravel\laravel-boolpress\resources\js\front.js */"./resources/js/front.js");
 
 
 /***/ })
